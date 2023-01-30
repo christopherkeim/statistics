555af2cc75879d5d8469940d84af50ecf57943e6
Christopher Keim <122534665+christopherkeim@users.noreply.github.com>
2023-01-30 15:04:11 -0500

    Create entropy.py

---
+++ entropy.py
@@ -0,0 +1,22 @@
+from math import log
+
+def entropy(self, samples):
+	"""
+	Takes a 2d nested list of samples, extracts a frequency count of the output variable values, them calculates the entropy for this group
+    of samples as entropy = Σ pi * log2(pi)   [from i=1 to n]
+	"""
+	#log2 function
+	def log2(x):
+		return log(x) / log(2)
+			
+	#store the frequency counts of the output variable values in this group of samples in a dictionary
+	counts = self.outputValueCounts(samples)
+		
+	#calculate the entropy
+	entropy = 0.0
+		
+	for value in counts:
+		p = float(counts[value] / len(samples))
+		entropy -= p * log2(p)
+			
+	return entropy
